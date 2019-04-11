8eb25cb440b9973ac03428e077f4c861bb34de77
Chris Luedtke <chrisluedtke@gmail.com>
2019-04-11 09:05:23 -0500

    make prediction

--- twitoff/app.py
+++ twitoff/app.py
@@ -1,15 +1,69 @@
 """Main application and routing logic for TwitOff."""
-from flask import Flask
-from .models import DB
+from os import getenv
+
+from dotenv import load_dotenv
+from sqlalchemy import func
+
+from flask import Flask, redirect, render_template, request, url_for
+from .models import DB, User
+from .twitter import add_or_update_user, update_all_users
+from .predict import predict_user
+
+
+load_dotenv()
+
 
 def create_app():
     """Create and configure an instance of the Flask application."""
     app = Flask(__name__)
-    app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///db.sqlite3'
+    app.config['SQLALCHEMY_DATABASE_URI'] = getenv('DATABASE_URL')
+    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+    app.config['ENV'] = getenv('FLASK_ENV')
     DB.init_app(app)
 
     @app.route('/')
     def root():
-        return 'Welcome to TwitOff!'
+        users = User.query.order_by(func.lower(User.name)).all()
+        return render_template('index.html', title='Home', 
+                               users=users)
+
+    @app.route('/update')
+    def update():
+        update_all_users()
+        return redirect(url_for('root'))
+
+    @app.route('/reset')
+    def reset():
+        DB.drop_all()
+        DB.create_all()
+        return redirect(url_for('root'))
+
+    @app.route('/user', methods=['POST'])
+    @app.route('/user/<name>', methods=['GET'])
+    def user(name=None, message=''):
+        name = name or request.values['user_name']
+        try:
+            if request.method == 'POST':
+                db_user = add_or_update_user(name)
+                message = f"User {db_user.name} successfully added!"
+            tweets = User.query.filter(User.name == name).one().tweets
+        except Exception as e:
+            message = "Error adding or fetching {}: {}".format(name, e)
+            tweets = []
+        return render_template('user.html', title=name, tweets=tweets,
+                               message=message)
+
+    @app.route('/compare', methods=['POST'])
+    def compare(message=''):
+        user1 = request.values['user1']
+        user2 = request.values['user2']
+        tweet_text = request.values['tweet_text']
+        if user1 == user2:
+            # TODO: error message
+            return redirect(url_for('root'))
+        else:
+            pre_dict = predict_user(user1, user2, tweet_text)
+        return render_template('prediction.html', title='Prediction', 
+                               tweet_text=tweet_text, pre_dict=pre_dict)
 
     return app
--- twitoff/models.py
+++ twitoff/models.py
@@ -1,23 +1,25 @@
-"""SQLAlchemy models for TwitOff."""
+"""SQLAlchemy models and utility functions for TwitOff."""
 from flask_sqlalchemy import SQLAlchemy
 
 DB = SQLAlchemy()
 
 class User(DB.Model):
-    """Twitter users that we pull and analyze tweets for."""
+    """Twitter users corresponding to Tweets in the Tweet table."""
     id = DB.Column(DB.BigInteger, primary_key=True)
     name = DB.Column(DB.String(15), nullable=False)
+    # Tweet IDs are ordinal ints, so can be used to fetch only more recent
+    newest_tweet_id = DB.Column(DB.BigInteger)
 
     def __repr__(self):
-        return '<User {}>'.format(self.name)
+        return f'<User {self.name}>'
 
 class Tweet(DB.Model):
-    """Tweets."""
+    """Tweets and their embeddings from Basilica."""
     id = DB.Column(DB.BigInteger, primary_key=True)
-    text = DB.Column(DB.Unicode(280))
+    text = DB.Column(DB.Unicode(300))  # Allowing for full + links
+    embedding = DB.Column(DB.PickleType, nullable=False)
     user_id = DB.Column(DB.BigInteger, DB.ForeignKey('user.id'), nullable=False)
     user = DB.relationship('User', backref=DB.backref('tweets', lazy=True))
 
     def __repr__(self):
-        return '<Tweet {}>'.format(self.text)
-
+        return f'<Tweet {self.text}>'
